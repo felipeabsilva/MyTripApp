43c9bc7781ea481cb302e1636734d97626c9a4a5
felipeabsilva <felipe_b97@yahoo.com.br>
2019-03-07 16:39:52 -0300

    First commit

---
+++ app/src/main/java/com/felipesilva/mytrip/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.felipesilva.mytrip
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //findViewById(R.id.edtAutonomy)
+
+    }
+}
